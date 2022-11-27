a453d94515d9647aef1983a1dca39e4c263ff218
Miguel Tablado <migueltablado@gmail.com>
2022-11-27 19:33:05 +0100

    webpack output management

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import * as _ from 'lodash';
+import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
+    const btn = document.createElement('button');
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
-};
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-console.log('miguelillo');
+    element.appendChild(btn);
+
+    return element;
+}
+
 document.body.appendChild(component());
